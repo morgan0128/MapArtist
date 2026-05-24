860bb4c6e9054a1f86cf12b7b2da0c837de9f717
Crane, Morgan M <morg28@iastate.edu>
2026-05-24 16:13:20 -0500

    Added class for "Color Picker" button, to appear as the fourth option at the bottom left in Screens/Map.

---
+++ MapArtistCode/NMapColorPickerButton.cs
@@ -0,0 +1,12 @@
+using BaseLib.Utils;
+using Godot;
+using MegaCrit.Sts2.Core.Nodes.GodotExtensions;
+
+
+namespace MapArtist.MapArtistCode;
+
+// [ScriptPath("res://src/Core/Nodes/Screens/Map/NMapDrawButton.cs")]
+public partial class NMapColorPickerButton : NButton
+{
+
+}
