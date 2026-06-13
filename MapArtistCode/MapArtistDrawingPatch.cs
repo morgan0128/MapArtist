fd9e7fa08fd485cdcbb8cdfd7cf5541f87039bdd
Crane, Morgan M <morg28@iastate.edu>
2026-06-13 11:43:07 -0500

    Fixed "tinted artifacts" eraser issue: changed eraser color and texture within the existing HarmonyPatch for CreateLineForPlayer.

--- MapArtistCode/MapArtistDrawingPatch.cs
+++ MapArtistCode/MapArtistDrawingPatch.cs
@@ -9,11 +9,20 @@ using MegaCrit.Sts2.Core.Nodes.Screens.Map;
 [HarmonyPatch(typeof(NMapDrawings), "CreateLineForPlayer", new Type[] { typeof(Player), typeof(bool) })]
 public static class MapArtistDrawingPatch
 {
+    private static Texture2D? _hardEraserTexture;
+
     private static void Postfix(Player player, bool isErasing, Line2D __result)
     {
         // NMapColorPickerButton.SetOwner __result.GetOwner();
-        if (isErasing || __result == null)
+        if (__result == null)
         {
+            return;
+        }
+
+        if (isErasing)
+        {
+            __result.DefaultColor = Colors.White;
+            __result.Texture = GetHardEraserTexture();
             return;
         }
 
@@ -27,5 +36,18 @@ public static class MapArtistDrawingPatch
             __result.Width = width;
         }
 
+    }
+
+    private static Texture2D GetHardEraserTexture()
+    {
+        if (_hardEraserTexture != null)
+        {
+            return _hardEraserTexture;
+        }
+
+        var image = Image.CreateEmpty(2, 2, false, Image.Format.Rgba8);
+        image.Fill(Colors.White);
+        _hardEraserTexture = ImageTexture.CreateFromImage(image);
+        return _hardEraserTexture;
     }
 }
