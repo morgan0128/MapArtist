7c076e83cad4af607c401a94b7902c42266f0f71
Crane, Morgan M <morg28@iastate.edu>
2026-07-01 15:40:37 -0500

    Late shift schedule at work very tired I am sticking with this general approach at least until get full functionality out of it. Easiest approach from here is probably harmony transpiler patch to ClearAllLinesForPlayer that replaces QueueFreeSafely with RemoveChildSafely (the Line2D children of drawViewport). If concerned about too many Line2D objects eventually piling up in scene without user being able to tell, can set a limit to total num of Line2D in player history/length of player history, or a similar limit that is instead a pool between all players.

--- MapArtistCode/MapArtistDrawingHistory.cs
+++ MapArtistCode/MapArtistDrawingHistory.cs
@@ -28,11 +28,11 @@ public sealed class MapArtistDrawingHistory
 
         public readonly bool IsClearOperation;
 
-        public CachedDrawingOperation(bool isClearOperation, Line2D? line, List<Line2D>? set = null)
+        public CachedDrawingOperation(bool isClearOperation, Line2D? line = null, List<Line2D>? set = null)
         {
             IsClearOperation = isClearOperation;
-            if ((line == null && set == null) || (line != null && set != null) ||
-                (!IsClearOperation && line == null)) return;
+            if ((line == null && set == null) || (line != null && set != null)) return;
+            if (!IsClearOperation && line == null) return; // A regular draw/erase operation contains one and only one line
 
             if (!IsClearOperation || Line != null)
             {
@@ -43,36 +43,27 @@ public sealed class MapArtistDrawingHistory
                 LineSet = set;
             }
         }
-    }
+
+        public bool IsValid()
+        {
+            return !((Line == null && LineSet == null) || (Line != null && LineSet != null) || 
+                     (!IsClearOperation && (Line == null || LineSet != null)));
+        }
+    }   
     
     private SubViewport? _localDrawViewport;
     private Stack<CachedDrawingOperation> _cachedOperations = new Stack<CachedDrawingOperation>();
     private Stack<CachedDrawingOperation> _cachedUndoneOperations = new Stack<CachedDrawingOperation>();
-
-    public bool PerformingOpClear = false;
-
-
     
-    // lock "redo line from history" command if:
-    //      _localDrawViewport is null
-    //      or
-    //      the player's _deletedLinesCache is empty
-    //      or
-    //      for most recent BeginLine where _drawingState.playerId == LocalPlayerId, drew a new line
-    // else: unlock
     private bool _localPlayerLastDrew = false;
 
+    private bool UndoLocked()
+    {
+        return (_localDrawViewport == null || _cachedOperations.Count == 0);
+    }
     private bool RedoLocked()
     {
         return (_localPlayerLastDrew || _localDrawViewport == null || _cachedUndoneOperations.Count == 0);
-    }
-
-    public void NotifyBeginLine(ulong drawingStatePlayerId, SubViewport drawingStateDrawViewport, Line2D line)
-    {
-        if (drawingStatePlayerId != Util.GetLocalPlayerId()) return;
-        
-        CheckUpdateLocalViewport(drawingStatePlayerId, drawingStateDrawViewport);
-        AddEntry(drawingStatePlayerId, line);
     }
 
     public void ResetState()
@@ -90,27 +81,6 @@ public sealed class MapArtistDrawingHistory
         }
         _cachedUndoneOperations.Clear();
     }
-
-    public void NotifyPlayerCleared(ulong drawingStatePlayerId, SubViewport drawingStateDrawViewport, List<Line2D> linesToSave)
-    {
-        if (drawingStatePlayerId != Util.GetLocalPlayerId() || linesToSave.Count == 0) return;
-        
-        PerformingOpClear = true;
-        CheckUpdateLocalViewport(drawingStatePlayerId, drawingStateDrawViewport);
-        if (linesToSave.Count == 1)
-        {
-            // for sake of memory management
-            var line = linesToSave[0];
-            var operation = new CachedDrawingOperation(PerformingOpClear, line);
-            _cachedOperations.Push(operation);
-        }
-        else
-        {
-            var operation = new CachedDrawingOperation(PerformingOpClear, null, linesToSave);
-            _cachedOperations.Push(operation);
-        }
-        PerformingOpClear = false;
-    }
     
     private void CheckUpdateLocalViewport(ulong playerId, SubViewport svp)
     {
@@ -121,61 +91,121 @@ public sealed class MapArtistDrawingHistory
         
         _localDrawViewport = svp;
     }
-
-    private void RemoveViewport()
+    
+    public void NotifyBeginLine(ulong drawingStatePlayerId, SubViewport drawingStateDrawViewport, Line2D line)
     {
-        // for where player leaves or game ends... do later... maybe.
-        // depends on approach (CheckUpdateLocalViewport)
+        if (drawingStatePlayerId != Util.GetLocalPlayerId()) return;
         
-                
+        CheckUpdateLocalViewport(drawingStatePlayerId, drawingStateDrawViewport);
+        CacheOperationDrewLine(drawingStatePlayerId, line);
     }
-
-    private void AddEntry(ulong playerId, Line2D? line)
+    
+    private CachedDrawingOperation? CacheOperationDrewLine(ulong playerId, Line2D? line)
     {
-        if (_localDrawViewport == null) return;
-        if (playerId != Util.GetLocalPlayerId()) return;
-        if (line == null) return;
-
-        var list = new List<Line2D> { line };
-        _existingLinesCache.Push(list);
+        if (_localDrawViewport == null) return null;
+        if (playerId != Util.GetLocalPlayerId()) return null;
+        if (line == null) return null;
+        
+        var operation = new CachedDrawingOperation(false, line);
+        
+        _cachedOperations.Push(operation);
         _localPlayerLastDrew = true;
+        return operation;
+    }
+    
+    private void ViewportAddLine(List<Line2D> toAdd)
+    {
+        foreach (var line in toAdd)
+        {
+            ViewportAddLine(line);
+        }
+    }
+    
+    private void ViewportAddLine(Line2D toAdd)
+    {
+        _localDrawViewport?.AddChildSafely((Node) toAdd);
+        
+        // TODO
+        // send message to perform same operation to other players
+
+        return;
+    }
+    
+    private void ViewportRemoveLine(List<Line2D> toRemove)
+    {
+        foreach (var line in toRemove)
+        {
+            ViewportRemoveLine(line);
+        }
+    }
+    
+    private void ViewportRemoveLine(Line2D toRemove)
+    {
+        _localDrawViewport?.RemoveChildSafely((Node) toRemove);
+        
+        // TODO
+        // send message to perform same operation to other players
+
+        return;
+    }
+    
+    public void NotifyPlayerCleared(ulong drawingStatePlayerId, SubViewport drawingStateDrawViewport, List<Line2D> linesToCache)
+    {
+        if (drawingStatePlayerId != Util.GetLocalPlayerId() || linesToCache.Count == 0) return;
+
+        CheckUpdateLocalViewport(drawingStatePlayerId, drawingStateDrawViewport);
+        // var reverseOrder = new Stack<CachedDrawingOperation>();
+        // while (_cachedUndoneOperations.Count > 0)
+        // {
+        //     reverseOrder.Push(_cachedUndoneOperations.Pop());
+        // }
+        // while (reverseOrder.Count > 0)
+        // {
+        //     _cachedOperations.Push(reverseOrder.Pop());
+        // }
+
+        if (linesToCache.Count == 1)
+        {
+            // for sake of memory management
+            var line = linesToCache[0];
+            var operation = new CachedDrawingOperation(true, line);
+            _cachedOperations.Push(operation);
+        }
+        else
+        {
+            var operation = new CachedDrawingOperation(true, null, linesToCache);
+            _cachedOperations.Push(operation);
+        }
+        
     }
 
     public void Undo()
     {
-        if (_localDrawViewport == null || _cachedOperations.Count == 0) return;
-        
+        if (UndoLocked()) return;
         var operation = _cachedOperations.Pop();
+        if (!operation.IsValid()) return;
+        
         if (operation.IsClearOperation)
         {
             if (operation.Line != null)
             {
-                AddLine(operation.Line);
-            }
-            else if (operation.LineSet != null)
-            {
-                foreach (var line in operation.LineSet)
-                {
-                    AddLine(line);
-                }
+                ViewportAddLine(operation.Line);
             }
             else
             {
-                return;
+                ViewportAddLine(operation.LineSet!);
             }
-
+            
             _cachedUndoneOperations.Push(operation);
         }
         else
         {
             if (operation.Line != null)
             {
-                // TODO begin at line 173
+                ViewportRemoveLine(operation.Line);
+                _cachedUndoneOperations.Push(operation);
             }
-            // regular undo drawn line operation
-            if (!RemoveLine(cached)) return; // error, but don't break 'undo history' by looping line back on stack
         }
-        _deletedLinesCache.Push(cached);
         _localPlayerLastDrew = false;
 
     }
@@ -183,58 +213,30 @@ public sealed class MapArtistDrawingHistory
     public void Redo()
     {
         if (RedoLocked()) return;
+        var operation = _cachedUndoneOperations.Pop();
+        if (!operation.IsValid()) return;
         
-        var cached = _deletedLinesCache.Pop();
-        if (cached.Count > 1)
+        if (operation.IsClearOperation)
         {
             // this entry represents a 'cleared set,' in which the clear operation has been undone
-            if (!RemoveLine(cached)) return; // error, but don't break 'redo history' by looping line back on stack
+            if (operation.Line != null)
+            {
+                ViewportRemoveLine(operation.Line);   
+            }
+            else
+            {
+                ViewportRemoveLine(operation.LineSet!);
+            }
+
         }
         else
         {
-            // regular redo drawn line operation
-            if (!AddLine(cached)) return; // error, but don't break 'redo history' by looping line back on stack
+            ViewportAddLine(operation.Line!);
         }
-        _existingLinesCache.Push(cached);
+        _cachedOperations.Push(operation);
         _localPlayerLastDrew = false;
     }
 
-    private bool RemoveLine(List<Line2D> toRemove)
-    {
-        foreach (var line in toRemove)
-        {
-            _localDrawViewport?.RemoveChildSafely(line);
-        }
-        
-        // TODO
-        // send message to perform same operation to other players
-        
-        return true;
-    }
-    
-    private bool AddLine(List<Line2D> toAdd)
-    {
-        bool failure = false;
-        foreach (var line in toAdd)
-        {
-            if (!AddLine(line))
-            {
-                failure = true;
-            }
-        }
-
-        return !failure;
-    }
-    
-    private bool AddLine(Line2D toAdd)
-    {
-        _localDrawViewport?.AddChildSafely((Node) toAdd);
-        
-        // TODO
-        // send message to perform same operation to other players
-
-        return true;
-    }
     
     // helper
     private void QueueFreeAllLines(CachedDrawingOperation operation)
